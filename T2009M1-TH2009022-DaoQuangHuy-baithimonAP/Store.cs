4e75b6770002ecd2970c3ddfdcee7db3376de87d
huy <huy@gmail.com>
2021-06-30 09:33:05 +0700

    upload T2009M1 AP exam exam TH2009022 Dao Quang Huy

---
+++ T2009M1-TH2009022-DaoQuangHuy-baithimonAP/Store.cs
@@ -0,0 +1,8 @@
+﻿namespace T2009M1_TH2009022_DaoQuangHuy_baithimonAP
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ T2009M1-TH2009022-DaoQuangHuy-baithimonAP/StoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T2009M1_TH2009022_DaoQuangHuy_baithimonAP
+{
+    public class StoreManager
+    {
+        private Dictionary<string, Store> _stores = new Dictionary<string, Store>();
+
+        public void AddProductRecords()
+        {
+            Console.WriteLine("Enter product ID: ");
+            var id = Console.ReadLine();
+            Console.WriteLine("Enter product name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter product price: ");
+            var price = double.Parse(Console.ReadLine());
+            _stores.Add(id, new Store()
+            {
+                Name = name,
+                Price = price
+            });
+        }
+        public void DisplayProductRecords ()
+        {
+            foreach (var list in _stores)
+            {
+                Console.WriteLine($"ID: {list.Key},{list.Value}");
+            }
+        }
+        public void DeleteProduct()
+        {
+            Console.WriteLine("Enter product ID: ");
+            var id = Console.ReadLine();
+            if (_stores.ContainsKey(id))
+            {
+                _stores.Remove(id);
+            }
+            else
+            {
+                Console.WriteLine("You must enter the product ID!");
+            }
+        }
+    }
+}
